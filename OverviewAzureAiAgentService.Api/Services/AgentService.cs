9ee44f9157900f387ffbb72efdafb70cd1f40fad
tallesdestefanidesouzavaliatti <talles.dsv@parsus.com>
2025-04-30 09:31:35 -0300

    Add document-based agent creation and file handling support
    Introduced functionality to create document-focused agents using a file search tool, leveraging vector stores for advanced file processing. Added helper methods for file uploads, vector store creation, and file management. Created a new `Constants` class to store static documentation files for model and file search tool guidance.

--- OverviewAzureAiAgentService.Api/Services/AgentService.cs
+++ OverviewAzureAiAgentService.Api/Services/AgentService.cs
@@ -10,85 +10,114 @@ namespace OverviewAzureAiAgentService.Api.Services;
 
 public class AgentService(IConfiguration configuration)
 {
-  private AgentsClient CreateAgentsClient()
-  {
-    var connectionString = configuration["AiServiceProjectConnectionString"]!;
-    return new AgentsClient(connectionString, new DefaultAzureCredential());
-  }
-  
-  public async Task<Agent> CreateAgentAsync(CreateAgentRequest request)
-  {
-    var aiModel = configuration["AiModel"]!;
-    var client = CreateAgentsClient();
+    private AgentsClient CreateAgentsClient()
+    {
+        var connectionString = configuration["AiServiceProjectConnectionString"]!;
+        return new AgentsClient(connectionString, new DefaultAzureCredential());
+    }
 
-    var agentResponse = await client.CreateAgentAsync(
-      model: aiModel,
-      name: request.Name,
-      instructions: request.Instructions);
+    public async Task<Agent> CreateAgentAsync(CreateAgentRequest request)
+    {
+        var aiModel = configuration["AiModel"]!;
+        var client = CreateAgentsClient();
 
-    return new Agent(
-      agentResponse.Value.Id,
-      agentResponse.Value.Name,
-      agentResponse.Value.Instructions);
-  }
-  
-  public async Task<Thread> CreateThreadAsync()
-  {
-    var client = CreateAgentsClient();
+        var agentResponse = await client.CreateAgentAsync(
+            model: aiModel,
+            name: request.Name,
+            instructions: request.Instructions);
 
-    var threadResponse = await client.CreateThreadAsync();
+        return new Agent(
+            agentResponse.Value.Id,
+            agentResponse.Value.Name,
+            agentResponse.Value.Instructions);
+    }
 
-    return new Thread(threadResponse.Value.Id);
-  }
-  
-  public async Task<Message> CreateRunAsync(CreateRunRequest request)
-  { 
-      var client = CreateAgentsClient();
-      
-      await client.CreateMessageAsync(
+    public async Task<Agent> CreateDocAgentAsync(CreateAgentRequest request)
+    {
+        var aiModel = configuration["AiModel"]!;
+        var client = CreateAgentsClient();
+        
+        var fileIds = new List<string>();
+
+        fileIds.Add(await UploadFileAsync(Constants.FileSearchDoc, nameof(Constants.FileSearchDoc)));
+        fileIds.Add(await UploadFileAsync(Constants.ModelSupportDoc, nameof(Constants.ModelSupportDoc)));
+        
+        var vectorStoreId = await CreateDocVectorStoreAsync(files: fileIds); 
+        
+        var fileSearchToolResource = new FileSearchToolResource();
+        fileSearchToolResource.VectorStoreIds.Add(vectorStoreId);
+        
+        var agentResponse = await client.CreateAgentAsync(
+            model: aiModel,
+            name: request.Name,
+            instructions: request.Instructions,
+            tools: new List<ToolDefinition> { new FileSearchToolDefinition() },
+            toolResources: new ToolResources() { FileSearch = fileSearchToolResource });
+        
+        return new Agent(
+            agentResponse.Value.Id,
+            agentResponse.Value.Name,
+            agentResponse.Value.Instructions);
+    }
+
+    public async Task<Thread> CreateThreadAsync()
+    {
+        var client = CreateAgentsClient();
+
+        var threadResponse = await client.CreateThreadAsync();
+
+        return new Thread(threadResponse.Value.Id);
+    }
+
+    public async Task<Message> CreateRunAsync(CreateRunRequest request)
+    {
+        var client = CreateAgentsClient();
+
+        await client.CreateMessageAsync(
             request.ThreadId,
             MessageRole.User,
             request.Message);
-              
-      Response<ThreadRun> runResponse = await client.CreateRunAsync(
+
+        Response<ThreadRun> runResponse = await client.CreateRunAsync(
             request.ThreadId,
             request.AgentId,
             additionalInstructions: "");
-        
-    do
-    {
-      await Task.Delay(TimeSpan.FromMilliseconds(500));
-      runResponse = await client.GetRunAsync(request.ThreadId, runResponse.Value.Id);
-      }
-      while (runResponse.Value.Status == RunStatus.Queued || runResponse.Value.Status == RunStatus.InProgress);
 
-      Response<PageableList<ThreadMessage>> afterRunMessagesResponse = await client.GetMessagesAsync(request.ThreadId, order: ListSortOrder.Descending, limit: 1);
-
-      var message = afterRunMessagesResponse.Value.Data.FirstOrDefault();
-
-      if (message is null)
-      {
-        throw new Exception("No messages found after run.");
-      }
-      
-      StringBuilder text = new();
-  
-      foreach (var contentItem in message.ContentItems)
-      {
-        if (contentItem is MessageTextContent textItem)
+        do
         {
-          text.AppendLine(textItem.Text);
-        }
-      }
-      
-      if (message.ContentItems.Count == 1 && text.Length > 0)
-      {
-        text.Length--;
-      }
+            await Task.Delay(TimeSpan.FromMilliseconds(500));
+            runResponse = await client.GetRunAsync(request.ThreadId, runResponse.Value.Id);
+        } while (runResponse.Value.Status == RunStatus.Queued || runResponse.Value.Status == RunStatus.InProgress);
 
-      return new Message(message.Id, message.Role.ToString(), text.ToString());
-  }
-  public async Task<IEnumerable<Message>> ListMessagesAsync(string threadId)
+        Response<PageableList<ThreadMessage>> afterRunMessagesResponse =
+            await client.GetMessagesAsync(request.ThreadId, order: ListSortOrder.Descending, limit: 1);
+
+        var message = afterRunMessagesResponse.Value.Data.FirstOrDefault();
+
+        if (message is null)
+        {
+            throw new Exception("No messages found after run.");
+        }
+
+        StringBuilder text = new();
+
+        foreach (var contentItem in message.ContentItems)
+        {
+            if (contentItem is MessageTextContent textItem)
+            {
+                text.AppendLine(textItem.Text);
+            }
+        }
+
+        if (message.ContentItems.Count == 1 && text.Length > 0)
+        {
+            text.Length--;
+        }
+
+        return new Message(message.Id, message.Role.ToString(), text.ToString());
+    }
+
+    public async Task<IEnumerable<Message>> ListMessagesAsync(string threadId)
     {
         var client = CreateAgentsClient();
 
@@ -113,5 +142,37 @@ public class AgentService(IConfiguration configuration)
 
             return new Message(message.Id, message.Role.ToString(), text.ToString());
         });
+    }
+
+    private async Task<string> UploadFileAsync(string content, string fileName)
+    {
+        var client = CreateAgentsClient();
+        
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+
+        var fileResponse = await client.UploadFileAsync(stream, AgentFilePurpose.Agents, fileName);
+
+        return fileResponse.Value.Id;
+    }
+    
+    private async Task AddFilesToVectorStoreAsync(string vectorStoreId, IList<string> fileIds)
+    {
+        var client = CreateAgentsClient();
+
+        foreach (var fileId in fileIds)
+        {
+            await client.CreateVectorStoreFileAsync(vectorStoreId, fileId);
+        }
+    }
+    
+    private async Task<string> CreateDocVectorStoreAsync(IList<string> files)
+    {
+        var client = CreateAgentsClient();
+
+        var vectorStore = await client.CreateVectorStoreAsync(fileIds: files, name: "my-docs");
+
+        await AddFilesToVectorStoreAsync(vectorStore.Value.Id, files);
+        
+        return vectorStore.Value.Id;
     }
 }
